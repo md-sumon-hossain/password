d5bbe26c02612737815346992e61108a7a157c3c
sumon <18103159@iubat.edu>
2022-03-30 15:07:34 +0600

    login form design

--- resources/views/backend/login/login.blade.php
+++ resources/views/backend/login/login.blade.php
@@ -4,23 +4,22 @@
     <link rel="stylesheet" href="{{ url('/backend/login/css/style.css') }}">
 </head>
 <body>
-    <form action="{{ route('dologin') }}" method="post">
+    <center>
+        <form action="{{ route('dologin') }}" method="post">
       
-        <div class="container">
-          <label for="uname"><b>Username</b></label>
-          <input type="text" placeholder="Enter Username" name="name" required>
-      
-          <label for="psw"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="password" required>
-      
-          <button type="submit">Login</button>
-        </div>
-      
-        <div class="container" style="background-color:#f1f1f1">
-          <button type="button" class="cancelbtn">Cancel</button>
-          <span class="psw">Forgot <a href="#">password?</a></span>
-        </div>
-      </form>
+            <div class="container">
+              <label for="uname"><b>USERNAME</b></label>
+              <input type="text" placeholder="Enter Username" name="name" required>
+          
+              <label for="psw"><b>PASSWORD</b></label>
+              <input type="password" placeholder="Enter Password" name="password" required>
+          
+              <button type="submit">LOGIN</button>
+              <a class="forgot" href="">FORGOT ?</a>
+            </div>
+          </form>
+    </center>
+    
 </body>
 </html>
 
