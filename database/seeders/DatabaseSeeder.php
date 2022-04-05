496e1622b982528a0c9532d2adb08fdf51c22064
sumon <18103159@iubat.edu>
2022-04-05 11:54:52 +0600

    guard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call([UserSeeder::class]);    }
+        $this->call([UserSeeder::class]); 
+        $this->call([EmployeeSeeder::class]);   
+    }
 }
---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Employee;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Employee::create([
+            'name'=>'zami',
+            'email'=>'zami@gmail.com',
+            'password'=>bcrypt('12345'),    
+        ]);
+    }
+}
