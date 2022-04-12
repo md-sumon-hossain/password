6a6a93156f363ea04ace66a1db853f454215f55c
sumon <18103159@iubat.edu>
2022-04-12 13:52:57 +0600

    git doc

---
+++ app/Http/Controllers/backend/GitController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GitController extends Controller
+{
+    #project upload
+
+    # git status
+    # git add .
+    # git commit -m "name"
+    # git push origin master/main
+
+
+    #git clone 
+
+    # git clone 'github link of the project'
+    # composer install
+    # cp .env.example .env 
+    # php artisan key:generate
+
+
+    #branch push
+
+    # git branch branchname
+    # git checkout branchname
+    # git status
+    # git add .
+    # git commit -m "name"
+    # git push origin branchname
+    # git checkout master
+    # git pull
+
+
+}
